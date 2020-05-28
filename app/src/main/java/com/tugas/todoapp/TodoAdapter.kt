2a32d1edc323d70e50575976b2c00978cde97111
Muhammad Nur Faisi S <faizi1234098@gmail.com>
2020-05-28 15:10:05 +0800

    Updated Date and Time Features

--- app/src/main/java/com/tugas/todoapp/TodoAdapter.kt
+++ app/src/main/java/com/tugas/todoapp/TodoAdapter.kt
@@ -10,6 +10,9 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.tugas.todoapp.database.Todo
 import com.tugas.todoapp.databinding.ListItemBinding
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class TodoAdapter(private val viewModel: TodoViewModel) :
@@ -36,7 +39,6 @@ class TodoAdapter(private val viewModel: TodoViewModel) :
         holder.btnDel.setOnClickListener {
             viewModel.removeTodo(getItem(holder.adapterPosition))
         }
-
         //Edit
         holder.btnEdit.setOnClickListener {
             val context = holder.itemView.context
@@ -46,22 +48,33 @@ class TodoAdapter(private val viewModel: TodoViewModel) :
         //Menganmbil data
             val prevTitle =getItem(position).title
             val prevTask = getItem(position).task
+            val prevDate = getItem(position).date
             val editTitle = view.findViewById<TextView>(R.id.editTitle)
             editTitle.text = prevTitle
             val editTask = view.findViewById<TextView>(R.id.editTask)
             editTask.text = prevTask
             val updateBtn = view.findViewById<TextView>(R.id.update_btn)
             val cancelBtn = view.findViewById<TextView>(R.id.cancel1_btn)
+            val dateUpdate = view.findViewById<TextView>(R.id.date_update)
+            dateUpdate.text = prevDate
+            val calendar = Calendar.getInstance()
+            val simpleFormat = SimpleDateFormat("hh:mm:ss a")
+            val time = simpleFormat.format(calendar.time)
+            val currentDate = DateFormat.getDateInstance(DateFormat.DEFAULT).format(calendar.time)
+            dateUpdate.setText("Updated : $currentDate $time")
             //Membuat dial9og
             val alertDialog = AlertDialog.Builder(context).setView(view).show()
                 updateBtn.setOnClickListener {
                     val editedTitle = editTitle.text.toString()
                     val editedTask = editTask.text.toString()
+                    val editedDate = dateUpdate.text.toString()
                     getItem(holder.adapterPosition).title = editedTitle
                     getItem(holder.adapterPosition).task = editedTask
+                    getItem(holder.adapterPosition).date = editedDate
                     viewModel.updateTodo(getItem(holder.adapterPosition))
                     holder.titleText.text = editedTitle
                     holder.todoText.text = editedTask
+                    holder.dateCrt.text = editedDate
                     alertDialog.dismiss()
                 }
                 cancelBtn.setOnClickListener{
